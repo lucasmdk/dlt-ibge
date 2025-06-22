545280c02a2195718aa4c7eff171890f9515daa8
lucasmk <lucasrd.araujo@outlook.com>
2025-06-22 13:00:05 -0300

    remove jdbc dependency, reading data directly from json file

--- pipelines/transform_silver.py
+++ pipelines/transform_silver.py
@@ -5,20 +5,13 @@ import duckdb
 import re
 import os
 
-# JAR path
-jdb_jar_path = os.path.abspath("drivers/duckdb_jdbc-0.10.2.jar")
-
 # Start SparkSession
 spark = SparkSession.builder \
     .appName("transform_silver") \
-    .config("spark.jars", jdb_jar_path) \
     .getOrCreate()
 
 # Read bronze data from DuckDB
-bronze_df = spark.read.format("jdbc") \
-    .option("url", "jdbc:duckdb:data/ibge_localidades.duckdb") \
-    .option("dbtable", "bronze.estados") \
-    .load()
+bronze_df = spark.read.option("multiline", "true").json("data/bronze/municipios.json")
 
 # Function to create medallion schema
 def create_medallion_schema(conn, schema_name: str):
@@ -26,31 +19,20 @@ def create_medallion_schema(conn, schema_name: str):
         CREATE SCHEMA IF NOT EXISTS {schema_name};
     """)
 
-# Function to extract nested data
-def extract_from_bronze(field: str, key: str) -> str:
-    if not field:
-        return None
-    match = re.search(fr"{key}=([^,}}]+)", field)
-    return match.group(1).strip() if match else None
-
-# User Defined Functions to extract fields
-extract_id_udf = udf(lambda x: extract_from_bronze(x, "id"), StringType())
-extract_nome_udf = udf(lambda x: extract_from_bronze(x, "nome"), StringType())
-extract_sigla_udf = udf(lambda x: extract_from_bronze(x, "sigla"), StringType())
-
 # Build DataFrame
-municipios_df = bronze_df \
-    .withColumn("municipio_id", col("id")) \
-    .withColumn("municipio_nome", col("nome")) \
-    .withColumn("microrregiao_id", extract_id_udf(col("microrregiao"))) \
-    .withColumn("microrregiao_nome", extract_nome_udf(col("microrregiao"))) \
-    .withColumn("mesorregiao_id", extract_id_udf(col("mesorregiao"))) \
-    .withColumn("mesorregiao_nome", extract_nome_udf(col("mesorregiao"))) \
-    .withColumn("uf_id", extract_id_udf(col("uf"))) \
-    .withColumn("uf_sigla", extract_sigla_udf(col("uf"))) \
-    .withColumn("uf_nome", extract_nome_udf(col("uf"))) \
-    .withColumn("regiao_id", extract_id_udf(col("regiao"))) \
-    .withColumn("regiao_nome", extract_nome_udf(col("regiao")))
+municipios_df = bronze_df.select (\
+    col("id").alias("municipio_id"),
+    col("nome").alias("municipio_nome"),
+    col("microrregiao.id").alias("microrregiao_id"),
+    col("microrregiao.nome").alias("microrregiao_nome"),
+    col("microrregiao.mesorregiao.id").alias("mesorregiao_id"),
+    col("microrregiao.mesorregiao.nome").alias("mesorregiao_nome"),
+    col("microrregiao.mesorregiao.UF.id").alias("uf_id"),
+    col("microrregiao.mesorregiao.UF.sigla").alias("uf_sigla"),
+    col("microrregiao.mesorregiao.UF.nome").alias("uf_nome"),
+    col("microrregiao.mesorregiao.UF.regiao.id").alias("regiao_id"),
+    col("microrregiao.mesorregiao.UF.regiao.nome").alias("regiao_nome")
+)
 
 # Save DataFrame to DuckDB
 municipios_df.write \
