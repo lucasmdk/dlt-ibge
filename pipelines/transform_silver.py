4b36a5bad0827d7c3bc58b8bf7f9ee415b9c5fcf
lucasmk <lucasrd.araujo@outlook.com>
2025-06-22 15:44:02 -0300

    build transform_silver.py

--- pipelines/transform_silver.py
+++ pipelines/transform_silver.py
@@ -1,17 +1,7 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, regexp_extract, udf
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+from pyspark.sql.functions import col
 import duckdb
-import re
 import os
-
-# Start SparkSession
-spark = SparkSession.builder \
-    .appName("transform_silver") \
-    .getOrCreate()
-
-# Read bronze data from DuckDB
-bronze_df = spark.read.option("multiline", "true").json("data/bronze/municipios.json")
 
 # Function to create medallion schema
 def create_medallion_schema(conn, schema_name: str):
@@ -19,31 +9,50 @@ def create_medallion_schema(conn, schema_name: str):
         CREATE SCHEMA IF NOT EXISTS {schema_name};
     """)
 
-# Build DataFrame
-municipios_df = bronze_df.select (\
-    col("id").alias("municipio_id"),
-    col("nome").alias("municipio_nome"),
-    col("microrregiao.id").alias("microrregiao_id"),
-    col("microrregiao.nome").alias("microrregiao_nome"),
-    col("microrregiao.mesorregiao.id").alias("mesorregiao_id"),
-    col("microrregiao.mesorregiao.nome").alias("mesorregiao_nome"),
-    col("microrregiao.mesorregiao.UF.id").alias("uf_id"),
-    col("microrregiao.mesorregiao.UF.sigla").alias("uf_sigla"),
-    col("microrregiao.mesorregiao.UF.nome").alias("uf_nome"),
-    col("microrregiao.mesorregiao.UF.regiao.id").alias("regiao_id"),
-    col("microrregiao.mesorregiao.UF.regiao.nome").alias("regiao_nome")
-)
+# Function to transform IBGE Localidades data
+def transform_localidades(spark: SparkSession):
+    # Define file paths
+    bronze_path = "data/bronze/municipios.json"
+    silver_path = "data/silver/municipios.parquet"
 
-# Save DataFrame to DuckDB
-municipios_df.write \
-    .format("jdbc") \
-    .option("url", "jdbc:duckdb:data/ibge_localidades.duckdb") \
-    .option("dbtable", "silver.estados") \
-    .mode("overwrite") \
-    .save()
+    # Read bronze data from DuckDB
+    bronze_df = spark.read.option("multiline", "true").json(bronze_path)
 
-# Save DataFrame as Parquet file
-municipios_df.write.mode("overwrite").parquet("data/silver/municipios.parquet")
+    # Build DataFrame
+    municipios_df = bronze_df.select (
+        col("id").alias("municipio_id"),
+        col("nome").alias("municipio_nome"),
+        col("microrregiao.id").alias("microrregiao_id"),
+        col("microrregiao.nome").alias("microrregiao_nome"),
+        col("microrregiao.mesorregiao.id").alias("mesorregiao_id"),
+        col("microrregiao.mesorregiao.nome").alias("mesorregiao_nome"),
+        col("microrregiao.mesorregiao.UF.id").alias("uf_id"),
+        col("microrregiao.mesorregiao.UF.sigla").alias("uf_sigla"),
+        col("microrregiao.mesorregiao.UF.nome").alias("uf_nome"),
+        col("microrregiao.mesorregiao.UF.regiao.id").alias("regiao_id"),
+        col("microrregiao.mesorregiao.UF.regiao.nome").alias("regiao_nome")
+    )
 
-spark.stop()
-print("Transformação concluída. Dados salvos em silver.estados")
+    # Verify silver folder presence. If not, creates it
+    if not os.path.exists("./data/silver"):
+        os.makedirs("./data/silver")
+
+    # Save DataFrame as Parquet file
+    municipios_df.write.mode("overwrite").parquet(silver_path)
+    print(f"Silver layer data created in: {silver_path}")
+
+# Main function
+if __name__ == "__main__":
+    # Start SparkSession
+    os.environ['HADOOP_HOME'] = 'C:\\hadoop'
+
+    spark = SparkSession.builder \
+    .appName("transform_silver") \
+    .getOrCreate()
+
+    try:
+        transform_localidades(spark)
+    except Exception as e:
+        print(f"Failed to transform silver layer: {e}")
+    finally:
+        spark.stop()
